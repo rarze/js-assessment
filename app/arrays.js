2f8641dc6bf70c7c5b5b0560861d1c3873354bc2
Rodrigo Arze <rodrigo.arze@jalasoft.com>
2019-04-02 19:42:22 -0400

    Started working with some array functions

--- app/arrays.js
+++ app/arrays.js
@@ -1,28 +1,33 @@
 arraysAnswers = {
   /**
    * Find the 0 based index of item in arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to find in arr
    * @returns {Number} The index of item in arr, or -1 if item is not in arr.
    */
   indexOf: function indexOf(arr, item) {
     // Implement a function, that returns the 0 based index of an element in an array.
+    return arr.indexOf(item);
   },
 
   /**
    * Determine the sum of the items of arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number} The numerical sum of all items in arr.
    */
   sum: function sum(arr) {
-
+    let accum = 0;
+    arr.forEach((number) => {
+      accum += number;
+    });
+    return accum;
   },
 
   /**
-   * Create a new array with the same items as arr, excluding item 
-   * 
+   * Create a new array with the same items as arr, excluding item
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be excluded from the new array
    * @returns {Number[]} A new array containing all numbers from arr except item.
@@ -33,50 +38,54 @@ arraysAnswers = {
 
   /**
    * Adds a number, item, to the end of an array, arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be appended to the end of arr
    * @returns {Number[]} The array arr, with item appended.
    */
   append: function append(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   /**
    * Removes a number, item, from the end of an array, arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} The array arr, with the last element removed..
    */
   truncate: function truncate(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   /**
    * Adds a number, item, to the beginning of an array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to append to the beginning of arr.
    * @returns {Number[]} The array arr, with the first element item added
    */
   prepend: function prepend(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
 
   /**
    * Removes the first element from an array
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} The array arr, with the first element item removed.
    */
   curtail: function curtail(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   /**
    * Combines the two arrays arr1 and arr2 together
-   * 
+   *
    * @param {Number[]} arr1 - An array of numbers
    * @param {Number[]} arr2 - An array of numbers
    * @returns {Number[]} A new array, with elements from arr1 and arr2 in that order.
@@ -87,7 +96,7 @@ arraysAnswers = {
 
   /**
    * Insert a number item into an array arr at the 0 based position index.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be inerted into arr
    * @param {Number} index - A 0 based index into the array arr.
@@ -99,7 +108,7 @@ arraysAnswers = {
 
   /**
    * Counts the number of times a number item appears in an array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to count in arr
    * @returns {Number} The count of the number of times the number item appeared in arr.
@@ -110,7 +119,7 @@ arraysAnswers = {
 
   /**
    * Determines the number of duplicated numbers in the array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} An array of numbers that appear in arr more than once.
    */
@@ -120,7 +129,7 @@ arraysAnswers = {
 
   /**
    * Squares each number in an array arr. Example: square([1, 2, 3]) returns [1, 4, 9].
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} A new array of numbers that contains the elements of arr squared.
    */
@@ -130,7 +139,7 @@ arraysAnswers = {
 
   /**
    * Finds the indices of the occurrences of a number target in an array of numbers arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} target - A number to find all occurences of.
    * @returns {Number[]} A new array of numbers which represent the indices of target in arr.
